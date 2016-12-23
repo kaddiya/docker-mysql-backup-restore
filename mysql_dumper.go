a6298f76483040d716f8330598786636fa59201d
kaddiya <kaddiya@gmail.com>
2016-12-23 18:43:48 +0530

    Formatted some stuff up

--- mysql_dumper.go
+++ mysql_dumper.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"github.com/kaddiya/docker-mysql-backup-restore/dump"
-	"os"
 	"fmt"
-	"time"
+	"github.com/kaddiya/docker-mysql-backup-restore/dump"
 	"github.com/kaddiya/docker-mysql-backup-restore/fileutils"
+	"os"
+	"time"
 )
 
 func main() {
@@ -31,49 +31,49 @@ func main() {
 		panic("the database name is not supplied")
 	}
 
-	var latestSqlDumpBasePath = fmt.Sprintf("%s/latest",os.Getenv("dump_path"))
-	var archivedSqlDumpBasePath = fmt.Sprintf("%s/archived",os.Getenv("dump_path"))
+	var latestSqlDumpBasePath = fmt.Sprintf("%s/latest", os.Getenv("dump_path"))
+	var archivedSqlDumpBasePath = fmt.Sprintf("%s/archived", os.Getenv("dump_path"))
 	fmt.Println(latestSqlDumpBasePath)
-	err1 := fileutils.CreateDirectoryIfNotExists(latestSqlDumpBasePath,0777)
+	err1 := fileutils.CreateDirectoryIfNotExists(latestSqlDumpBasePath, 0777)
 
-	if err1 != nil{
+	if err1 != nil {
 		fmt.Println("error in checking the existence of the latest dump directory")
 	}
-	err2 := fileutils.CreateDirectoryIfNotExists(archivedSqlDumpBasePath,0777)
-	if err2 != nil{
+	err2 := fileutils.CreateDirectoryIfNotExists(archivedSqlDumpBasePath, 0777)
+	if err2 != nil {
 		fmt.Println("error in checking the existence of the archived dump directory")
 	}
 
 	t := time.Now()
-  var archivedDumpFileName = fileutils.GetFullyQualifiedPathOfFile(fmt.Sprintf("%s/archived",os.Getenv("dump_path")),fmt.Sprintf("%d-%s-%d-%d:%d.sql",t.Day(),t.Month(),t.Year(),t.Hour(),t.Minute()))
+	var archivedDumpFileName = fileutils.GetFullyQualifiedPathOfFile(fmt.Sprintf("%s/archived", os.Getenv("dump_path")), fmt.Sprintf("%d-%s-%d-%d:%d.sql", t.Day(), t.Month(), t.Year(), t.Hour(), t.Minute()))
 
-	latestDumpFilePath := fileutils.GetFullyQualifiedPathOfFile(latestSqlDumpBasePath,"backup.sql")
-	errorFilePath := fileutils.GetFullyQualifiedPathOfFile(latestSqlDumpBasePath,"error.log")
+	latestDumpFilePath := fileutils.GetFullyQualifiedPathOfFile(latestSqlDumpBasePath, "backup.sql")
+	errorFilePath := fileutils.GetFullyQualifiedPathOfFile(latestSqlDumpBasePath, "error.log")
 
 	//execute it
 	errorBuf, outputBuf := dumper.MysqlDump()
 
-//write the latest
-fileutils.WriteToFile(latestDumpFilePath,outputBuf.Bytes())
-//write error log
-fileutils.WriteToFile(errorFilePath,errorBuf.Bytes())
-//write the archive itself
-fileutils.WriteToFile(archivedDumpFileName,outputBuf.Bytes())
+	//write the latest
+	fileutils.WriteToFile(latestDumpFilePath, outputBuf.Bytes())
+	//write error log
+	fileutils.WriteToFile(errorFilePath, errorBuf.Bytes())
+	//write the archive itself
+	fileutils.WriteToFile(archivedDumpFileName, outputBuf.Bytes())
 
-/*	ferr := ioutil.WriteFile(filePath, outputBuf.Bytes(), 0644)
-	if ferr != nil {
-		panic(ferr)
-	}
-	//write the logs
-	ferr1 := ioutil.WriteFile(errorFilePath, errorBuf.Bytes(), 0644)
-	if ferr1 != nil {
-		panic(ferr1)
-	}
+	/*	ferr := ioutil.WriteFile(filePath, outputBuf.Bytes(), 0644)
+		if ferr != nil {
+			panic(ferr)
+		}
+		//write the logs
+		ferr1 := ioutil.WriteFile(errorFilePath, errorBuf.Bytes(), 0644)
+		if ferr1 != nil {
+			panic(ferr1)
+		}
 
-	//write it to the archived folder as well
-	arErr := ioutil.WriteFile(archivedDumpFileName,outputBuf.Bytes(),0644)
-	if arErr !=nil {
-		panic(arErr)
-	}*/
+		//write it to the archived folder as well
+		arErr := ioutil.WriteFile(archivedDumpFileName,outputBuf.Bytes(),0644)
+		if arErr !=nil {
+			panic(arErr)
+		}*/
 
 }
