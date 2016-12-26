aba423dacde1de0589e0106f6f93ee1d5c1edb0f
kaddiya <kaddiya@gmail.com>
2016-12-26 20:54:59 +0530

    Changes to be debugged latesr

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,9 @@ RUN apt-get update && \
     apt-get install -y ca-certificates && \
     mkdir /backups
 
-ADD docker-mysql-backup-restore_linux_amd64 /start.sh
-RUN chmod +x /start.sh
+ADD docker-mysql-backup-restore_linux_amd64 /execute.sh
+RUN chmod +x /execute.sh
 
 VOLUME ["/backups"]
 
-ENTRYPOINT ["/start.sh"]
+ENTRYPOINT ["/execute.sh"]
--- restore/restorer.go
+++ restore/restorer.go
@@ -8,17 +8,22 @@ import(
 )
 func RestoreFromFile(pathToFile string){
 
-  args := make([]string, 0, 16)
-  args = append(args, fmt.Sprintf(" --host=%s",os.Getenv("dumper_db_host")))
-  args = append(args, fmt.Sprintf(" --port=%s",os.Getenv("dumper_db_port")))
-  args = append(args, fmt.Sprintf(" --user=%s ",os.Getenv("dumper_db_user")))
-  args = append(args, fmt.Sprintf(" -p",os.Getenv("dumper_db_password")))
-  args = append(args, fmt.Sprintf(" %s ",os.Getenv("dumper_db_name")))
-  args = append(args,fmt.Sprintf(" < %s",pathToFile))
+ args := make([]string, 0, 16)
+  args = append(args, fmt.Sprintf("-h%s",os.Getenv("dumper_db_host")))
+  args = append(args, fmt.Sprintf("-P%s",os.Getenv("dumper_db_port")))
+  args = append(args, fmt.Sprintf("-u%s ",os.Getenv("dumper_db_user")))
+  args = append(args, fmt.Sprintf("-p%s",os.Getenv("dumper_db_password")))
+  args = append(args, fmt.Sprintf("%s",os.Getenv("dumper_db_name")))
+  args = append(args,"<")
+  args = append(args,fmt.Sprintf("%s",pathToFile))
 
   cmd := exec.Command("mysql", args...)
   cmd.Stderr = os.Stderr
   cmd.Stdout = os.Stdout
-  cmd.Run()
+  err := cmd.Run()
+
+  if err !=nil{
+    fmt.Println("error : ",err)
+  }
 
 }
