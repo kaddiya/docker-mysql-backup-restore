b333360af4bb4a3a5ccd975535bc03a297356b8f
kaddiya <kaddiya@gmail.com>
2016-12-26 20:23:44 +0530

    Stabilised somewhat

--- restore/restorer.go
+++ restore/restorer.go
@@ -9,12 +9,12 @@ import(
 func RestoreFromFile(pathToFile string){
 
   args := make([]string, 0, 16)
-  args = append(args, fmt.Sprintf("--host=%s",os.Getenv("dumper_db_host")))
-  args = append(args, fmt.Sprintf("--port=%s",os.Getenv("dumper_db_port")))
-  args = append(args, fmt.Sprintf("--user=%s",os.Getenv("dumper_db_user")))
-  args = append(args, fmt.Sprintf("--password=",os.Getenv("dumper_db_password")))
-  args = append(args, fmt.Sprintf("--user=%s",os.Getenv("dumper_db_user")))
-  args = append(args, fmt.Sprintf("%s < %s",os.Getenv("dumper_db_name"),pathToFile))
+  args = append(args, fmt.Sprintf(" --host=%s",os.Getenv("dumper_db_host")))
+  args = append(args, fmt.Sprintf(" --port=%s",os.Getenv("dumper_db_port")))
+  args = append(args, fmt.Sprintf(" --user=%s ",os.Getenv("dumper_db_user")))
+  args = append(args, fmt.Sprintf(" -p",os.Getenv("dumper_db_password")))
+  args = append(args, fmt.Sprintf(" %s ",os.Getenv("dumper_db_name")))
+  args = append(args,fmt.Sprintf(" < %s",pathToFile))
 
   cmd := exec.Command("mysql", args...)
   cmd.Stderr = os.Stderr
