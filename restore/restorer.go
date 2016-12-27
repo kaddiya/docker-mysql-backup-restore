74ef6c9d2d9214e5993bd47fc872fb3829eb2729
kaddiya <kaddiya@gmail.com>
2016-12-27 12:46:53 +0530

    Created the args by appending

--- restore/restorer.go
+++ restore/restorer.go
@@ -15,18 +15,16 @@ func RestoreFromFile(pathToFile string){
   r := bytes.NewReader(b)
 
   if err !=nil {
-    fmt.Println("some werroe")
+    panic(err)
   }
 
-  //args := make([]string, 0, 16)
-/*  args = append(args, fmt.Sprintf("-h%s ",os.Getenv("dumper_db_host")))
-  args = append(args, fmt.Sprintf("-P%s ",os.Getenv("dumper_db_port")))
-  args = append(args, fmt.Sprintf("-u%s ",os.Getenv("dumper_db_user")))
-  args = append(args, fmt.Sprintf("-p%s ",os.Getenv("dumper_db_password")))
-  args = append(args, fmt.Sprintf("%s ",os.Getenv("dumper_db_name")))
-*/
-  //args := []string{"-h" ,"127.0.0.1", "-u", "deploy ","-p","deploy " ,"proof"}
-  args := []string{"-h127.0.0.1", "-udeploy" ,"-pdeploy", "proof"}
+  args := make([]string, 0, 16)
+  args = append(args, fmt.Sprintf("-h%s",os.Getenv("dumper_db_host")))
+  args = append(args, fmt.Sprintf("-P%s",os.Getenv("dumper_db_port")))
+  args = append(args, fmt.Sprintf("-u%s",os.Getenv("dumper_db_user")))
+  args = append(args, fmt.Sprintf("-p%s",os.Getenv("dumper_db_password")))
+  args = append(args, fmt.Sprintf("%s",os.Getenv("dumper_db_name")))
+
   mysqlCmd := exec.Command("mysql",args...)
   mysqlCmd.Stdin = r
   mysqlCmd.Stderr = os.Stderr
@@ -35,7 +33,7 @@ func RestoreFromFile(pathToFile string){
   err2 :=  mysqlCmd.Run()
 
   if err2 !=nil{
-    fmt.Println(err2)
+    panic(err2)
   }
 
 
