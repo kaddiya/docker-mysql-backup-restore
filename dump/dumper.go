7ff60eac66838a1050b47996acb534c9769940bc
kaddiya <kaddiya@gmail.com>
2017-01-23 20:52:17 +0530

    Renamed

--- dump/dumper.go
+++ dump/dumper.go
@@ -1,7 +1,6 @@
 package dumper
 
 import(
-  "fmt"
   "os/exec"
   "bytes"
 )
@@ -9,18 +8,10 @@ import(
 func MysqlDump(args []string) (stdErrBuf,stdOutBuf bytes.Buffer){
 
   var dumpOutBuf, dumperrBuf bytes.Buffer
-  dumpArgs := make([]string, len(args)+1)
-  copy(dumpArgs,args[:len(args)-1])
-  dumpArgs = append(dumpArgs,"--add-drop-database")
-  dumpArgs = append(dumpArgs,"--databases")
-  dumpArgs = append(dumpArgs,args[len(args)-1])
+   cmd := exec.Command("mysqldump", args...)
+   cmd.Stderr = &dumperrBuf
+   cmd.Stdout = &dumpOutBuf
+   cmd.Run()
 
-  fmt.Println(dumpArgs)
-
-  cmd := exec.Command("mysqldump", dumpArgs...)
-  cmd.Stderr = &dumperrBuf
-  cmd.Stdout = &dumpOutBuf
-  cmd.Run()
-
-  return dumperrBuf,dumpOutBuf
+   return dumperrBuf,dumpOutBuf
 }
