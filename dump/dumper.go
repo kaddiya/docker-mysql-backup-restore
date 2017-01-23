7c9cd271a065c1b50ad40713f1c92ed9ba759f2a
kaddiya <kaddiya@gmail.com>
2017-01-23 19:48:22 +0530

    Commited teh renamed changes

--- dump/dumper.go
+++ dump/dumper.go
@@ -1,6 +1,7 @@
 package dumper
 
 import(
+  "fmt"
   "os/exec"
   "bytes"
 )
@@ -8,7 +9,15 @@ import(
 func MysqlDump(args []string) (stdErrBuf,stdOutBuf bytes.Buffer){
 
   var dumpOutBuf, dumperrBuf bytes.Buffer
-  cmd := exec.Command("mysqldump", args...)
+  dumpArgs := make([]string, len(args)+1)
+  copy(dumpArgs,args[:len(args)-1])
+  dumpArgs = append(dumpArgs,"--add-drop-database")
+  dumpArgs = append(dumpArgs,"--databases")
+  dumpArgs = append(dumpArgs,args[len(args)-1])
+
+  fmt.Println(dumpArgs)
+
+  cmd := exec.Command("mysqldump", dumpArgs...)
   cmd.Stderr = &dumperrBuf
   cmd.Stdout = &dumpOutBuf
   cmd.Run()
--- restore/restorer.go
+++ restore/restorer.go
@@ -4,12 +4,15 @@ package restore
 import(
   "os/exec"
   "os"
+  "fmt"
   "bytes"
 )
 
 func RestoreFromFile(fileContent []byte,args []string){
   r := bytes.NewReader(fileContent)
-
+  //fmt.Println(args)
+  args = args[:len(args)-1]
+  fmt.Println(args)
   mysqlCmd := exec.Command("mysql",args...)
   mysqlCmd.Stdin = r
   mysqlCmd.Stderr = os.Stderr
